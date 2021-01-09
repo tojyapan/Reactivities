6df8bf3a70cd4f5a3dca37b4c677bc3d29ff25ec
tojyapan <tojyapan925@gmail.com>
2021-01-09 20:54:32 +0900

    Adding the Activity Details component

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Grid, List } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { ActivityList } from './ActivityList';
+import { ActivityDetails } from '../details/ActivityDetails';
 
 interface IProps {
     activities: IActivity[]
@@ -12,11 +13,9 @@ export const ActivityDashboard: React.FC<IProps> = ({activities}) => {
         <Grid>
             <Grid.Column width={10}>
                 <ActivityList activities={activities}/>
-                {/* <List>
-                    {activities.map(activity => (
-                        <List.Item key={activity.id}>{activity.title}</List.Item>
-                    ))}
-                </List> */}
+            </Grid.Column>
+            <Grid.Column width={6}>
+                <ActivityDetails />
             </Grid.Column>
         </Grid>
     );

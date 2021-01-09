d86711bf122931ee3511bf9137d51ade1b9fc518
tojyapan <tojyapan925@gmail.com>
2021-01-09 18:11:07 +0900

    Adding the Activity Dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import { Container, Header, Icon, List } from 'semantic-ui-react'
 import axios from 'axios';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
 
 const App = () =>  {
 
@@ -20,11 +21,7 @@ const App = () =>  {
     <Fragment>
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <List>
-          {activities.map(activity => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
+        <ActivityDashboard activities={activities} />
       </Container>
     </Fragment>
   );

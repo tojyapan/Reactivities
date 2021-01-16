131ea6703efc5a49568fa9832df1f7405e33f514
tojyapan <tojyapan925@gmail.com>
2021-01-16 23:21:26 +0900

    Refactoring the select activity function

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,17 +1,17 @@
 import { observer } from 'mobx-react-lite';
-import React, { SyntheticEvent } from 'react';
+import React, { SyntheticEvent, useContext } from 'react';
 import { Item, Button, Label, Segment } from 'semantic-ui-react';
-import { IActivity } from '../../../app/models/activity';
+import ActivityStore from '../../../app/stores/activityStore';
 
 interface IProps {
-    activities: IActivity[];
-    selectActivity: (id: string) => void;
     deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
     submitting: boolean;
     target: string;
 };
 
-const ActivityList: React.FC<IProps> = ({activities, selectActivity, deleteActivity, submitting, target}) => {
+const ActivityList: React.FC<IProps> = ({deleteActivity, submitting, target}) => {
+    const activityStore = useContext(ActivityStore);
+    const {activities, selectActivity} = activityStore;
     return (
         <Segment clearing>
             <Item.Group divided>
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import { observer } from 'mobx-react-lite';
+import React, { useContext } from 'react'
 import { Card, Image, Button } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
+import ActivityStore from '../../../app/stores/activityStore';
 
 interface IProps {
-    activity: IActivity;
     setEditMode: (editMode: boolean) => void;
     setSelectedActivity: (activity: IActivity | null) => void;
 }
 
-export const ActivityDetails: React.FC<IProps> = ({activity, setEditMode, setSelectedActivity}) => {
+const ActivityDetails: React.FC<IProps> = ({setEditMode, setSelectedActivity}) => {
+    const activityStore = useContext(ActivityStore);
+    const {selectedActivity: activity} = activityStore;
     return (
         <Card fluid>
-            <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
+            <Image src={`/assets/categoryImages/${activity!.category}.jpg`} wrapped ui={false} />
             <Card.Content>
-            <Card.Header>{activity.title}</Card.Header>
+            <Card.Header>{activity!.title}</Card.Header>
             <Card.Meta>
-                <span>{activity.date}</span>
+                <span>{activity!.date}</span>
             </Card.Meta>
             <Card.Description>
-                {activity.description}
+                {activity!.description}
             </Card.Description>
             </Card.Content>
             <Card.Content extra>
@@ -28,5 +31,7 @@ export const ActivityDetails: React.FC<IProps> = ({activity, setEditMode, setSel
                 </Button.Group>
             </Card.Content>
         </Card>
-    )
-}
+    );
+};
+
+export default observer(ActivityDetails);
